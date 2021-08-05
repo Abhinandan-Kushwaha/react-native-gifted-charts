4ac85d2b4bb3f0b68b1e36a0c704aee01717d6d1
Abhinandan Kushwaha <kushabhi5@gmail.com>
2021-08-06 00:03:18 +0530

    Added margins to chart area

--- src/BarChart/styles.tsx
+++ src/BarChart/styles.tsx
@@ -3,6 +3,8 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     width: '100%',
+    marginBottom: 40,
+    marginRight: 40,
   },
   horizBar: {
     flexDirection: 'row',
