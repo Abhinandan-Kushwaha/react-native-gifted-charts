dfc37ff1a77894996241fd2c03bf15b3053c5ecd
Abhinandan Kushwaha <kushabhi5@gmail.com>
2022-01-12 19:20:31 +0530

    Fixed issue with hideOrigin in Line charts

--- src/LineChart/index.tsx
+++ src/LineChart/index.tsx
@@ -1061,16 +1061,22 @@ export const LineChart = (props: propTypes) => {
                           marginBottom: stepHeight / -2,
                         },
                       ]}>
-                      {showFractionalValues
+                      {hideOrigin
+                        ? index === horizSections.length - 1
+                          ? ''
+                          : showFractionalValues
+                          ? sectionItems.value
+                            ? sectionItems.value
+                            : '0'
+                          : sectionItems.value
+                          ? sectionItems.value.toString().split('.')[0]
+                          : '0'
+                        : showFractionalValues
                         ? sectionItems.value
                           ? sectionItems.value
-                          : hideOrigin
-                          ? ''
                           : '0'
                         : sectionItems.value
                         ? sectionItems.value.toString().split('.')[0]
-                        : hideOrigin
-                        ? ''
                         : '0'}
                     </Text>
                   ) : null}
