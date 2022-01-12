937872b1b98ec3b02608d249d788ae0a4101345a
Abhinandan Kushwaha <kushabhi5@gmail.com>
2022-01-12 16:52:27 +0530

    Added smooth transition animations for change in data iin Line and Area Charts

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 import {Alert} from 'react-native';
 import {View, Text, StyleSheet} from 'react-native';
@@ -6,15 +6,43 @@ import {BarChart, LineChart} from './src';
 
 const App = () => {
   const [toggle, setToggle] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentData(latestData5);
+    }, 1100 + 100 + 310);
+    setTimeout(() => {
+      setCurrentData(latestData2);
+    }, 1100 + 300 + 620);
+
+    setTimeout(() => {
+      setCurrentData(latestData5);
+    }, 1100 + 500 + 930);
+
+    setTimeout(() => {
+      setCurrentData(latestData3);
+    }, 1100 + 1000 + 1240);
+
+    setTimeout(() => {
+      setCurrentData(latestData4);
+    }, 1100 + 1500 + 1550);
+
+    setTimeout(() => {
+      setCurrentData(latestData5);
+    }, 6000);
+
+    setTimeout(() => {
+      setCurrentData(latestData);
+    }, 7000);
+  }, []);
   const dPoint = () => {
     return (
       <View
         style={{
-          width: 20,
-          height: 20,
+          width: 14,
+          height: 14,
           backgroundColor: 'white',
-          borderWidth: 4,
-          borderRadius: 10,
+          borderWidth: 3,
+          borderRadius: 7,
           borderColor: '#07BAD1',
         }}
       />
@@ -29,24 +57,24 @@ const App = () => {
   };
   const latestData = [
     {
-      value: 100,
+      value: 600 - 100,
       labelComponent: () => lcomp('22 Nov'),
       customDataPoint: dPoint,
     },
     {
-      value: 140,
+      value: 600 - 120,
       hideDataPoint: true,
     },
     {
-      value: 250,
+      value: 600 - 210,
       customDataPoint: dPoint,
     },
     {
-      value: 290,
+      value: 600 - 250,
       hideDataPoint: true,
     },
     {
-      value: 410,
+      value: 600 - 320,
       labelComponent: () => lcomp('24 Nov'),
       customDataPoint: dPoint,
       showStrip: true,
@@ -69,31 +97,377 @@ const App = () => {
       dataPointLabelShiftX: -14,
     },
     {
-      value: 440,
+      value: 600 - 310,
       hideDataPoint: true,
     },
     {
-      value: 300,
+      value: 600 - 270,
       customDataPoint: dPoint,
     },
     {
-      value: 280,
+      value: 600 - 240,
       hideDataPoint: true,
     },
     {
-      value: 180,
+      value: 600 - 130,
       labelComponent: () => lcomp('26 Nov'),
       customDataPoint: dPoint,
     },
     {
-      value: 150,
+      value: 600 - 120,
       hideDataPoint: true,
     },
     {
-      value: 150,
+      value: 600 - 100,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 210,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 270,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 240,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 100,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 210,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 20,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 100,
       customDataPoint: dPoint,
     },
   ];
+
+  const latestData2 = [
+    {
+      value: 400 - 100,
+      labelComponent: () => lcomp('22 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 210,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 250,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 320,
+      labelComponent: () => lcomp('24 Nov'),
+      customDataPoint: dPoint,
+      showStrip: true,
+      stripHeight: 190,
+      stripColor: 'black',
+      dataPointLabelComponent: () => {
+        return (
+          <View
+            style={{
+              backgroundColor: 'black',
+              paddingHorizontal: 8,
+              paddingVertical: 5,
+              borderRadius: 4,
+            }}>
+            <Text style={{color: 'white'}}>410</Text>
+          </View>
+        );
+      },
+      dataPointLabelShiftY: -70,
+      dataPointLabelShiftX: -14,
+    },
+    {
+      value: 400 - 310,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 270,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 240,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 130,
+      labelComponent: () => lcomp('26 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 100,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 210,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 270,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 240,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 100,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 210,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 400 - 20,
+      hideDataPoint: true,
+    },
+    {
+      value: 400 - 100,
+      customDataPoint: dPoint,
+    },
+  ];
+
+  const latestData3 = [
+    {
+      value: 600 - 170,
+      labelComponent: () => lcomp('22 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 60,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 290,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 390,
+      labelComponent: () => lcomp('24 Nov'),
+      customDataPoint: dPoint,
+      showStrip: true,
+      stripHeight: 190,
+      stripColor: 'black',
+      dataPointLabelComponent: () => {
+        return (
+          <View
+            style={{
+              backgroundColor: 'black',
+              paddingHorizontal: 8,
+              paddingVertical: 5,
+              borderRadius: 4,
+            }}>
+            <Text style={{color: 'white'}}>410</Text>
+          </View>
+        );
+      },
+      dataPointLabelShiftY: -70,
+      dataPointLabelShiftX: -14,
+    },
+    {
+      value: 600 - 210,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 370,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 140,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 330,
+      labelComponent: () => lcomp('26 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 50,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 200,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 110,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 370,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 140,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 320,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 20,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 310,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 600 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 600 - 100,
+      customDataPoint: dPoint,
+    },
+  ];
+
+  const latestData4 = [
+    {
+      value: 300 - 170,
+      labelComponent: () => lcomp('22 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 60,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 290,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 390,
+      labelComponent: () => lcomp('24 Nov'),
+      customDataPoint: dPoint,
+      showStrip: true,
+      stripHeight: 190,
+      stripColor: 'black',
+      dataPointLabelComponent: () => {
+        return (
+          <View
+            style={{
+              backgroundColor: 'black',
+              paddingHorizontal: 8,
+              paddingVertical: 5,
+              borderRadius: 4,
+            }}>
+            <Text style={{color: 'white'}}>410</Text>
+          </View>
+        );
+      },
+      dataPointLabelShiftY: -70,
+      dataPointLabelShiftX: -14,
+    },
+    {
+      value: 300 - 210,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 370,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 140,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 330,
+      labelComponent: () => lcomp('26 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 50,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 200,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 110,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 370,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 140,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 320,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 20,
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 310,
+      labelComponent: () => lcomp('28 Nov'),
+      customDataPoint: dPoint,
+    },
+    {
+      value: 300 - 120,
+      hideDataPoint: true,
+    },
+    {
+      value: 300 - 100,
+      customDataPoint: dPoint,
+    },
+  ];
+  const latestData5 = [];
+  for (let i = 0; i < latestData.length; i++) {
+    latestData5[i] = {...latestData, value: 300};
+  }
+
+  const [currentData, setCurrentData] = useState(latestData);
 
   const customLabel = val => {
     return (
@@ -154,35 +528,68 @@ const App = () => {
   ]);
 
   return (
-    <View
-      style={{
-        marginTop: 100,
-        paddingVertical: 50,
-        backgroundColor: '#414141',
-      }}>
-      <LineChart
-        thickness={6}
-        color="#07BAD1"
-        maxValue={600}
-        noOfSections={3}
-        areaChart
-        yAxisTextStyle={{color: 'lightgray'}}
-        data={latestData}
-        curved
-        startFillColor={'rgb(84,219,234)'}
-        endFillColor={'rgb(84,219,234)'}
-        startOpacity={0.4}
-        endOpacity={0.4}
-        spacing={38}
-        backgroundColor="#414141"
-        rulesColor="gray"
-        rulesType="solid"
-        initialSpacing={10}
-        yAxisColor="lightgray"
-        xAxisColor="lightgray"
-        dataPointsHeight={20}
-        dataPointsWidth={20}
-      />
+    <View>
+      <View
+        style={{
+          marginVertical: 100,
+          paddingVertical: 50,
+          backgroundColor: '#414141',
+        }}>
+        <LineChart
+          isAnimated
+          thickness={3}
+          color="#07BAD1"
+          maxValue={600}
+          noOfSections={3}
+          animateOnDataChange
+          animationDuration={1000}
+          onDataChangeAnimationDuration={300}
+          areaChart
+          yAxisTextStyle={{color: 'lightgray'}}
+          data={currentData}
+          // curved
+          hideDataPoints
+          startFillColor={'rgb(84,219,234)'}
+          endFillColor={'rgb(84,219,234)'}
+          startOpacity={0.4}
+          endOpacity={0.1}
+          spacing={22}
+          backgroundColor="#414141"
+          rulesColor="gray"
+          rulesType="solid"
+          initialSpacing={10}
+          yAxisColor="lightgray"
+          xAxisColor="lightgray"
+          dataPointsHeight={20}
+          dataPointsWidth={20}
+        />
+      </View>
+
+      {/* <TouchableOpacity
+        onPress={() => setCurrentData(latestData)}
+        style={{padding: 12, backgroundColor: 'lightgreen', marginBottom: 15}}>
+        <Text>Smooth</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setCurrentData(latestData2)}
+        style={{padding: 12, backgroundColor: 'lightgreen', marginBottom: 15}}>
+        <Text>Smooth Low</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setCurrentData(latestData3)}
+        style={{padding: 12, backgroundColor: 'lightgreen', marginBottom: 15}}>
+        <Text>Edged</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setCurrentData(latestData4)}
+        style={{padding: 12, backgroundColor: 'lightgreen', marginBottom: 15}}>
+        <Text>Edged Low</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setCurrentData(latestData5)}
+        style={{padding: 12, backgroundColor: 'lightgreen'}}>
+        <Text>Straight</Text>
+      </TouchableOpacity> */}
 
       {/* {!toggle ? (
         <BarChart
