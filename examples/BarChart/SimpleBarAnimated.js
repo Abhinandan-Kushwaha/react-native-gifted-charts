5a40b24b0aa052492f1b32d5f0c4dc38b0a4db15
Abhinandan Kushwaha <kushabhi5@gmail.com>
2023-07-02 15:59:10 +0530

    upgraded to version 1.3.0 updated the tests and fixed warnings in Pie and 3D Bar charts

--- examples/BarChart/SimpleBarAnimated.js
+++ examples/BarChart/SimpleBarAnimated.js
@@ -6,7 +6,7 @@ const SimpleBarAnimated = () => {
   const barData = [
     {value: 230, label: 'Jan', frontColor: '#4ABFF4'},
     {value: 180, label: 'Feb', frontColor: '#79C3DB'},
-    {value: 195, label: 'Mar', frontColor: '#28B2B3'},
+    {value: 200, label: 'Mar', frontColor: '#28B2B3'},
     {value: 250, label: 'Apr', frontColor: '#4ADDBA'},
     {value: 320, label: 'May', frontColor: '#91E3E3'},
   ];
