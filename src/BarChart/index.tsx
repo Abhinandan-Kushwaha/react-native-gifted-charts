8b1c4db2c5091b05d76cfdcc0307d58b5ffeecc2
Abhinandan Kushwaha <kushabhi5@gmail.com>
2023-11-02 22:02:28 +0530

    fixed bug with scrollX

--- src/BarChart/index.tsx
+++ src/BarChart/index.tsx
@@ -565,7 +565,7 @@ export const BarChart = (props: BarChartPropsType) => {
       pointerStripColor,
       pointerConfig,
       pointerLabelComponent,
-      scrollX:0,
+      scrollX: 0,
     });
   };
 
